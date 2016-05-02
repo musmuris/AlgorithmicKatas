af222e5c6b4ff7b9993641ba77a4a568d77cb59b
Nigel Morse <nigelm@zepler.org>
2016-05-02 20:04:37 +0100

    FrogRiver solution

---
+++ FrogRiver/FrogRiver/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogRiver
+{
+    class Program
+    {
+        static int CrossTheRiver(int X, int[] A)
+        {
+            HashSet<int> positions = new HashSet<int>(Enumerable.Range(1,X));
+            for( int i = 0; i < A.Length; ++i)
+            {
+                positions.Remove(A[i]);
+                if (positions.Count == 0)
+                    return i;
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            // Should be 6
+            int[] A = {1, 3, 1, 4, 2, 3, 5, 4};
+            Console.Out.WriteLine(CrossTheRiver(5, A));
+
+            // should be 0
+            int[] A2 = { 1 };
+            Console.Out.WriteLine(CrossTheRiver(1, A2));
+
+            // should be -1
+            int[] A3 = { 1, 1 };
+            Console.Out.WriteLine(CrossTheRiver(2, A2));
+        }
+    }
+}
---
+++ FrogRiver/README.md
@@ -0,0 +1,6 @@
+Frog River
+===============
+
+This taken from https://codility.com/demo/take-sample-test/frog_river_one/
+
+Not coppied here as the site wants it timed, so I don't wish to spoil the surprise :)
